05d9ded0bdeab2b07abb12ba277739b8ce655bd2
Fabio M. Matos <fabiomonteirom37@gmail.com>
2021-11-08 14:55:08 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 import logo from './logo.svg';
 
 function App() {
   return (
-    <div >
-
-    <h1 className="text-primary">Hello World</h1>
-    <h1 className="text-success">Olá Mundo !</h1>
-    <h1 className="text-success">שלום עולם</h1>
-    
-
-        </div>
+    <>
+      <NavBar />
+      <div className="container" >
+        <h1 className="text-primary">Hello World</h1>
+        <h1 className="text-success">Olá Mundo !</h1>
+        <h1 className="text-success">שלום עולם</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 

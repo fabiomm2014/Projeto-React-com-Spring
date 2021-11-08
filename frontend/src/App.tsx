4c49c8edb962a75d85cc5429df9c075460c3f442
Fabio M. Matos <fabiomonteirom37@gmail.com>
2021-11-08 12:07:53 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <div >
 
-    <h1>Hello World</h1>
+    <h1 className="text-primary">Hello World</h1>
+    <h1 className="text-success">Olá Mundo !</h1>
+    <h1 className="text-success">שלום עולם</h1>
     
+
         </div>
   );
 }
